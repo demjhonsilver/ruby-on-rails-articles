2a204f9ba1368a0141229582cd7fbf43af285d2e
demjhonsilver <djsilverph@gmail.com>
2023-10-13 23:19:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center">
+
+<img src="https://raw.githubusercontent.com/demjhonsilver/ruby-on-rails-articles/main/public/screenshot.png" alt="Logo" width="800" height="450"/>
+
+</p>
---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  
+  root :to => 'articles#index'
+  get    '/articles',             to:    'articles#index'
+  post   '/articles',             to:    'articles#create'
+  get    '/articles/new',         to:    'articles#new', as: 'new_article'
+  get    '/articles/:id/edit',    to:    'articles#edit', as: 'edit_article'
+  get    '/articles/:id',         to:    'articles#show', as: 'article'
+  patch  '/articles/:id',         to:    'articles#update'
+  put    '/articles/:id',         to:    'articles#update'
+  delete '/articles/:id',         to:    'articles#delete'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
